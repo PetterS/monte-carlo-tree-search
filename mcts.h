e972d9a77d717b5600f9281bfb7410e9c53c50bb
Petter Strandmark <petter.strandmark@jeppesen.com>
2013-08-27 17:52:06 +0200

    Working for Nim.

--- mcts.h
+++ mcts.h
@@ -5,14 +5,32 @@
 #ifndef MCTS_HEADER_PETTER
 #define MCTS_HEADER_PETTER
 
+#include <cstdlib>
 #include <iostream>
+#include <random>
+#include <set>
+#include <sstream>
 #include <string>
 #include <vector>
+
+namespace MCTS
+{
+using std::cerr;
+using std::endl;
 using std::vector;
 using std::size_t;
 
-namespace MCTS
-{	
+std::mt19937_64 random_engine(std::mt19937_64::default_seed);
+
+void check(bool expr, const char* message);
+void assertion_failed(const char* expr, const char* file, int line);
+
+#define attest(expr) if (!(expr)) { ::MCTS::assertion_failed(#expr, __FILE__, __LINE__); }
+#ifndef NDEBUG
+	#define dattest(expr) if (!(expr)) { ::MCTS::assertion_failed(#expr, __FILE__, __LINE__); }
+#else
+	#define dattest(expr) ((void)0)
+#endif
 
 template<typename State>
 class Node
@@ -20,20 +38,244 @@ class Node
 public:
 	typedef typename State::Move Move;
 
-	Node();
+	Node(const State& state);
 	~Node();
 
-	Node* select_child() const;
-	Node* add_child();
+	bool has_untried_moves() const;
+	Move get_untried_move() const;
+	Move best_move() const;
+
+	bool has_children() const
+	{
+		return ! children.empty();
+	}
+
+	Node* select_child_UCT() const;
+	Node* add_child(const Move& move, const State& state);
 	void update(double result);
 
 	std::string to_string() const;
-	std::string tree_to_string() const;
+	std::string tree_to_string(int max_depth = 1000000, int indent = 0) const;
+
+	const Move move;
+	Node* const parent;
+	const int player_to_move;
+	double wins;
+	int visits;
+
+	std::vector<Move> moves;
+	mutable std::vector<Node*> children;
 
 private:
+	Node(const State& state, const Move& move, Node* parent);
+
 	std::string indent_string(int indent) const;
-	std::string children_to_string() const;
+
+	Node(const Node&);
+	Node& operator = (const Node&);
 };
+
+template<typename State>
+typename State::Move compute_move(const State& state, int player_just_moved);
+
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+
+template<typename State>
+Node<State>::Node(const State& state) :
+	move(State::no_move),
+	parent(nullptr),
+	player_to_move(state.player_to_move),
+	wins(0),
+	visits(0),
+	moves(state.get_moves())
+{ }
+
+template<typename State>
+Node<State>::Node(const State& state, const Move& move_, Node* parent_) :
+	move(move_),
+	parent(parent_),
+	player_to_move(state.player_to_move),
+	wins(0),
+	visits(0),
+	moves(state.get_moves())
+{ }
+
+template<typename State>
+Node<State>::~Node()
+{ }
+
+template<typename State>
+bool Node<State>::has_untried_moves() const
+{
+	return ! moves.empty();
+}
+
+template<typename State>
+typename State::Move Node<State>::get_untried_move() const
+{
+	attest( ! moves.empty());
+	std::uniform_int_distribution<std::size_t> moves_distribution(0, moves.size() - 1);
+	return moves[moves_distribution(random_engine)];
+}
+
+template<typename State>
+typename State::Move Node<State>::best_move() const
+{
+	attest(moves.empty());
+
+	std::sort(children.begin(), children.end(),
+		[](Node* a, Node* b) { return a->visits < b->visits; });
+	
+	return children.back()->move;
+}
+
+template<typename State>
+Node<State>* Node<State>::select_child_UCT() const
+{
+	attest( ! children.empty() );
+
+	auto key = [&] (Node* other)
+	{
+		return double(other->wins) / double(other->visits) +
+			std::sqrt(2.0 * std::log(this->visits) / other->visits);
+	};
+
+	return *std::max_element(children.begin(), children.end(),
+		[&key](Node* a, Node* b) { return key(a) < key(b); });
+}
+
+template<typename State>
+Node<State>* Node<State>::add_child(const Move& move, const State& state)
+{
+	auto node = new Node(state, move, this);
+	children.push_back(node);
+	attest( ! children.empty());
+
+	auto itr = moves.begin();
+	for (; itr != moves.end() && *itr != move; ++itr);
+	attest(itr != moves.end());
+	moves.erase(itr);
+	return node;
+}
+
+template<typename State>
+void Node<State>::update(double result)
+{
+	visits++;
+	wins += result;
+}
+
+template<typename State>
+std::string Node<State>::to_string() const
+{
+	std::stringstream sout;
+	sout << "["
+	     << "P" << 3 - player_to_move << " "
+	     << "M:" << move << " "
+	     << "W/V: " << wins << "/" << visits << " "
+	     << "U: " << moves.size() << "]\n";
+	return sout.str();
+}
+
+template<typename State>
+std::string Node<State>::tree_to_string(int max_depth, int indent) const
+{
+	if (indent >= max_depth) {
+		return "";
+	}
+
+	std::string s = indent_string(indent) + to_string();
+	for (auto child: children) {
+		s += child->tree_to_string(max_depth, indent + 1);
+	}
+	return s;
+}
+
+template<typename State>
+std::string Node<State>::indent_string(int indent) const
+{
+	std::string s = "";
+	for (int i = 1; i <= indent; ++i) {
+		s += "| ";
+	}
+	return s;
+}
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+
+template<typename State>
+typename State::Move compute_move(const State& root_state,
+                                  const int max_iterations = 10000)
+{
+	// Will support more players later.
+	attest(root_state.player_to_move == 1 || root_state.player_to_move == 2);
+	Node<State> root(root_state);
+
+	for (int iter = 1; iter <= max_iterations; ++iter) {
+		auto node = &root;
+		State state = root_state;
+
+		while (!node->has_untried_moves() && node->has_children()) {
+			node = node->select_child_UCT();
+			state.do_move(node->move);
+		}
+
+		if (node->has_untried_moves()) {
+			auto move = node->get_untried_move();
+			state.do_move(move);
+			node = node->add_child(move, state);
+		}
+
+		while (state.has_moves()) {
+			state.do_random_move();
+		}
+
+		while (node != nullptr) {
+			node->update(state.get_result(node->player_to_move));
+			node = node->parent;
+		}
+	}
+
+	std::cerr << root.tree_to_string(4);
+	std::cerr << endl;
+	std::cerr << root.tree_to_string(2);
+
+	return root.best_move();
+}
+
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+
+void check(bool expr, const char* message)
+{
+	if (!expr) {
+		throw std::invalid_argument(message);
+	}
+}
+
+void assertion_failed(const char* expr, const char* file_cstr, int line)
+{
+	using namespace std;
+
+	// Extract the file name only.
+	string file(file_cstr);
+	auto pos = file.find_last_of("/\\");
+	if (pos == string::npos) {
+		pos = 0;
+	}
+	file = file.substr(pos + 1);  // Returns empty string if pos + 1 == length.
+
+	stringstream sout;
+	sout << "Assertion failed: " << expr << " in " << file << ":" << line << ".";
+	throw runtime_error(sout.str().c_str());
+}
 
 }
 
--- tests/test_mcts.cpp
+++ tests/test_mcts.cpp
@@ -7,7 +7,125 @@
 
 using namespace std;
 
-TEST_CASE("dummy")
+// Player 1 has two options:
+//		1: Draw.
+//		2: Nothing happens (Player 2's turn).
+//
+// Player 2 has five options:
+//		  1: Player 1 wins.
+//		2-5: Player X wins. (default: 2)
+//
+// If X == 1, player 1 should play 2 for a guaranteed win after the next move by player 2.
+// If X == 2, player 1 should play 1 for an immediate draw.
+class TestGame
 {
+public:
+	typedef int Move;
+	static const Move no_move = -1;
+
+	TestGame(int X_ = 2)
+		: player_to_move(1),
+	      winner(-1),
+		  X(X_)
+	{ }
+
+	void do_move(Move move)
+	{
+		if (player_to_move == 1) {
+			attest(move >= 1 && move <= 2);
+
+			if (move == 1) {
+				winner = 0;
+			}
+			else {
+			}
+		}
+		else if (player_to_move == 2) {
+			attest(move >= 1 && move <= 5);
+
+			if (move == 1) {
+				winner = 1;
+			}
+			else {
+				winner = X;
+			}
+		}
+
+		player_to_move = 3 - player_to_move;
+	}
+
+	void do_random_move()
+	{
+		if (player_to_move == 1) {
+			std::uniform_int_distribution<Move> moves(1, 2);
+			do_move(moves(MCTS::random_engine));
+		}
+		else if (player_to_move == 2) {
+			std::uniform_int_distribution<Move> moves(1, 5);
+			do_move(moves(MCTS::random_engine));
+		}
+		
+	}
+
+	bool has_moves() const
+	{
+		return winner < 0;
+	}
+
+	std::vector<Move> get_moves() const
+	{
+		std::vector<Move> moves;
+		if ( ! has_moves()) {
+			return moves;
+		}
+
+		if (player_to_move == 1) {
+			moves.push_back(1);
+			moves.push_back(2);
+		}
+		else if (player_to_move == 2) {
+			moves.push_back(1);
+			moves.push_back(2);
+			moves.push_back(3);
+			moves.push_back(4);
+			moves.push_back(5);
+		}
+
+		return moves;
+	}
+
+	double get_result(int current_player_to_move) const
+	{
+		attest(winner >= 0);
+
+		if (winner == 0) {
+			return 0.5;
+		}
+
+		if (winner == current_player_to_move) {
+			return 0.0;
+		}
+		else {
+			return 1.0;
+		}
+	}
+
+	int player_to_move;
+	int winner;
+private:
+	int X;
+};
+
+TEST_CASE("dummy1")
+{
+	TestGame state(1);
+	auto move = MCTS::compute_move(state, 1000);
+	CHECK(move == 2);
 }
 
+TEST_CASE("dummy2")
+{
+	TestGame state(2);
+	auto move = MCTS::compute_move(state, 1000);
+	CHECK(move == 1);
+}
