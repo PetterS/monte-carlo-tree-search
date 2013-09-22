5d5082d9b30095517a5571ca137e257181f4c2b8
Petter Strandmark <petter.strandmark@gmail.com>
2013-09-22 12:30:56 +0200

    Start of Go game.

---
+++ games/go.cpp
@@ -0,0 +1,290 @@
+// Petter Strandmark 2013.
+
+#include <list>
+#include <sstream>
+#include <vector>
+
+#define UNICODE
+#include <cinder/app/AppBasic.h>
+#include <cinder/gl/TextureFont.h>
+
+#include <mcts.h>
+
+#include "go.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+// We'll create a new Cinder Application by deriving from the AppBasic class
+class GoApp: public AppBasic
+{
+  public:
+	GoApp();
+	void setup();
+	
+	void mouseDown( MouseEvent event );
+	void keyDown( KeyEvent event );
+	void update();
+	void draw();
+
+	static const int M = 9;
+	static const int N = 9;
+
+	static const int board_x = 25;
+	static const int board_y = 25;
+	static const int board_width = 40;
+
+	GoState<M, N> state;
+
+	enum PlayerType {HUMAN, COMPUTER};
+	PlayerType player1, player2;
+	MCTS::ComputeOptions player1_options, player2_options;
+
+	enum GameStatus {WAITING_FOR_USER, COMPUTER_THINKING, GAME_OVER, GAME_ERROR};
+	GameStatus game_status;
+	std::string error_string;
+
+	// Compute move.
+	std::future<GoState<M, N>::Move> computed_move;
+	void next_player();
+	void start_compute_move();
+	void check_for_computed_move();
+
+	// Drawing text.
+	Font				mFont;
+	gl::TextureFontRef	mTextureFont;
+};
+
+GoApp::GoApp()
+{ }
+
+void GoApp::setup()
+{
+	//state = GoState<M, N>(board);
+	player1 = COMPUTER;
+	player2 = COMPUTER;
+
+	player1_options.max_iterations = -1;
+	player1_options.max_time = 2.0;
+
+	player2_options.max_iterations = -1;
+	player2_options.max_time = 0.5;
+
+	if (player1 == HUMAN) {
+		game_status = WAITING_FOR_USER;
+	}
+	else {
+		start_compute_move();
+	}
+
+	mFont = Font("Times New Roman", 24 );
+	mTextureFont = gl::TextureFont::create( mFont );
+}
+
+void GoApp::start_compute_move()
+{
+	//game_status = WAITING_FOR_USER;
+	//return;
+
+	game_status = COMPUTER_THINKING;
+
+	MCTS::ComputeOptions options;
+	if (state.player_to_move == 1) {
+		options = player1_options;
+	}
+	else {
+		options = player2_options;
+	}
+
+	GoState<M, N> state_copy = state;
+	computed_move = 
+		std::async(std::launch::async,
+			[state_copy, options]() 
+			{ 
+				//auto best_move = MCTS::compute_move(state_copy, options);
+				//return best_move;
+
+				auto moves = state_copy.get_moves();
+				if (moves.size() == 1) {
+					return moves[0];
+				}
+
+				auto tree = MCTS::compute_tree(state_copy, options, 1241 * std::time(0));
+				typedef MCTS::Node<GoState<M, N>> Node;
+				auto best_child = *std::max_element( tree->children.begin(), tree->children.end(), [](Node* lhs, Node* rhs) { return lhs->visits < rhs->visits; } );
+				return best_child->move;
+			});
+}
+
+void GoApp::check_for_computed_move()
+{
+	//return;
+
+	if (game_status != COMPUTER_THINKING) {
+		return;
+	}
+
+	auto status = computed_move.wait_for(std::chrono::seconds(0));
+	if (status == std::future_status::ready) {
+		try {
+			auto move = computed_move.get();
+			state.do_move(move);
+
+			// Are there any more moves possible?
+			if (state.get_moves().empty()) {
+				game_status = GAME_OVER;
+			}
+			else {
+				next_player();
+			}
+
+		} catch (std::exception& error) {
+			game_status = GAME_ERROR;
+			error_string = error.what();
+		}
+	}
+}
+
+void GoApp::next_player()
+{
+	if (state.player_to_move == 1) {
+		if (player1 == HUMAN) {
+			game_status = WAITING_FOR_USER;
+		}
+		else {
+			start_compute_move();
+		}
+	}
+	else if (state.player_to_move == 2) {
+		if (player2 == HUMAN) {
+			game_status = WAITING_FOR_USER;
+		}
+		else {
+			start_compute_move();
+		}
+	}
+}
+
+void GoApp::mouseDown(MouseEvent event)
+{
+	if (game_status != WAITING_FOR_USER) {
+		return;
+	}
+
+	int x = event.getX();
+	int j = (x - board_x + board_width/2) / board_width;
+	int y = event.getY();
+	int i = (y - board_y + board_width/2) / board_width;
+
+	try {
+		if (state.is_move_possible(i, j)) {
+			state.do_move(GoState<M, N>::ij_to_ind(i, j));
+
+			// Are there any more moves possible?
+			if (state.get_moves().empty()) {
+				game_status = GAME_OVER;
+			}
+			else {
+				next_player();
+			}
+		}
+	} catch (std::exception& error) {
+		game_status = GAME_ERROR;
+		error_string = error.what();
+	}
+}
+
+void GoApp::keyDown( KeyEvent event )
+{
+	if (event.getChar() == 'p') {
+		if (game_status == WAITING_FOR_USER) {
+			state.do_move(GoState<M, N>::pass);
+
+			// Computer makes the next move.
+			start_compute_move();
+		}
+	}
+	else if (event.getChar() == 'r') {
+		state = GoState<M, N>();
+		setup();
+	}
+	else if (event.getChar() == 'd') {
+		state.dump_board("board.txt");
+		error_string = "Board dumped.";
+		game_status = GAME_ERROR;
+	}
+}
+
+void GoApp::update()
+{
+	check_for_computed_move();
+}
+
+void GoApp::draw()
+{
+	gl::clear( Color( 0.9f, 0.5f, 0.05f ) );
+	
+	gl::color( 0.0f, 0.0f, 0.0f );
+	for (int i = 0; i < M; ++i) {
+		int x  = board_x + i * board_width;
+		int y1 = board_y;
+		int y2 = board_y + (M-1) * board_width;
+		gl::drawLine(Vec2f(x, y1), Vec2f(x, y2));
+
+		int y  = board_y + i * board_width;
+		int x1 = board_x;
+		int x2 = board_x + (N-1) * board_width;
+		gl::drawLine(Vec2f(x1, y), Vec2f(x2, y));
+	}
+
+	for (int i = 0; i < M; ++i) {
+	for (int j = 0; j < N; ++j) {
+		Vec2f pos(board_x + j * board_width, board_y + i * board_width );
+
+		if ( ! state.is_move_possible(i, j)) {
+			gl::color( 1.0f, 0.0f, 0.0f );	
+			gl::drawSolidCircle(pos, 4.0f );
+		}
+
+		if (state.get_pos(i, j) == GoState<M, N>::player1) {
+			gl::color( 0.0f, 0.0f, 0.0f );
+			gl::drawSolidCircle(pos, board_width / 2 - 1);
+		}
+		else if (state.get_pos(i, j) == GoState<M, N>::player2) {
+			gl::color( 1.0f, 1.0f, 1.0f );
+			gl::drawSolidCircle(pos, board_width / 2 - 1);
+		}
+	}}
+
+	stringstream sout;
+	if (game_status == WAITING_FOR_USER) {
+		sout << "Make your move!" << endl;
+	}
+	else if (game_status == COMPUTER_THINKING) {
+		sout << "Computing..." << endl;
+	}
+	else if (game_status == GAME_OVER) {
+		sout << "Game over!" << endl;
+	}
+	else if (game_status == GAME_ERROR) {
+		sout << "ERROR:\n" << error_string << endl;
+	}
+
+	sout << "Player 1 (black) score: " << state.get_player_score(1) << endl;
+	sout << "Player 2 (white) score: " << state.get_player_score(2) << endl;
+
+	//sout << "Board hash: " << state.previous_board_hash_value << " -> " << state.compute_hash_value() << endl;
+	//for (auto move: state.get_moves()) {
+	//	sout << move << " ";
+	//}
+
+	Rectf boundsRect(board_x, board_y + M * board_width + 20, 
+	                 getWindowWidth() - 40, getWindowHeight() - 40 );
+	gl::enableAlphaBlending();
+	gl::color( ColorA( 0, 0, 0, 1.0f ) );
+	mTextureFont->drawStringWrapped( sout.str(), boundsRect );
+}
+
+// This line tells Cinder to actually create the application
+CINDER_APP_BASIC( GoApp, RendererGl )
---
+++ games/go.h
@@ -0,0 +1,401 @@
+// Petter Strandmark 2013
+// petter.strandmark@gmail.com
+
+#include <algorithm>
+#include <iostream>
+#include <utility>
+
+#include <mcts.h>
+
+template<unsigned int M, unsigned int N>
+class GoState
+{
+public:
+
+	// Mutable because is_move_possible temporary modifies
+	// the board.
+	mutable unsigned char board[M][N];
+	unsigned int previous_board_hash_value;
+	std::set<unsigned int> all_hash_values;
+	
+
+public:
+	static const unsigned char empty = 0;
+	static const unsigned char player1 = 1;
+	static const unsigned char player2 = 2;
+
+	mutable int depth;
+	int player_to_move;
+	typedef int Move;
+	static const Move no_move = -2;
+	static const Move pass    = -1;
+
+	static int ij_to_ind(int i, int j)
+	{
+		attest(i >= 0 && j >= 0 && i < M && j < N);
+		return N*i + j;
+	}
+
+	static std::pair<int, int> ind_to_ij(int ind)
+	{ 
+		attest(ind >= 0 && ind < M * N);
+		return std::make_pair(ind / N, ind % N); 
+	}
+
+
+	GoState():
+		player_to_move(1),
+		previous_board_hash_value(0),
+		depth(0)
+	{ 
+		all_hash_values.insert(compute_hash_value());
+
+		for (int i = 0; i < M; ++i) {
+			for (int j = 0; j < N; ++j) {
+				board[i][j] =  empty;
+			}
+		}
+	}
+
+	GoState(char board[M][N+1]):
+		player_to_move(1),
+		previous_board_hash_value(0),
+		depth(0)
+	{
+		GoState<M, N> state;
+		for (int i = 0; i < M; ++i) {
+		for (int j = 0; j < N; ++j) {
+			if (board[i][j] == '1') {
+				set_pos(i, j, 1);
+			}
+			else if (board[i][j] == '2') {
+				set_pos(i, j, 2);
+			}
+			else {
+				set_pos(i, j, empty);
+			}
+		}}
+	}
+
+	unsigned char get_pos(int i, int j) const
+	{
+		attest(ij_to_ind(i, j) >= 0);
+		return board[i][j];
+	}
+
+	void set_pos(int i, int j, unsigned char player)
+	{
+		attest(ij_to_ind(i, j) >= 0);
+		board[i][j] = player;
+	}
+
+	unsigned int compute_hash_value() const
+	{
+		unsigned int value = 0;
+		for (int i = 0; i < M; ++i) {
+		for (int j = 0; j < N; ++j) {
+			value = 65537 * value + board[i][j];
+		}}
+		return value;
+	}
+
+	bool is_move_possible(int i, int j) const
+	{
+		return is_move_possible(i, j, player_to_move);
+	}
+
+	bool is_move_possible(const int i, const int j, const int player) const
+	{
+		const int opponent = 3 - player;
+
+		if (0 <= i && i < M && 0 <= j && j < N) {
+			if (board[i][j] != empty) {
+				return false;
+			}
+
+			// See if it is possible to move into this empty place.
+			std::set<std::pair<int, int>> pieces;
+			board[i][j] = player;
+
+			bool possible = false;
+			if (is_alive(i, j, &pieces)) {
+				// This stone is immediately alive.
+				possible = true;
+			}
+			else {
+				// This stone may have killed opponents so
+				// that it is alive.
+
+				if (i > 0 && board[i - 1][j] == opponent && !is_alive(i - 1, j, &pieces)) {
+					possible = true;
+				}
+				else if (i < M - 1 && board[i + 1][j] == opponent && !is_alive(i + 1, j, &pieces)) {
+					possible = true;
+				}
+				else if (j > 0 && board[i][j - 1] == opponent && !is_alive(i, j - 1, &pieces)) {
+					possible = true;
+				}
+				else if (j < N - 1 && board[i][j + 1] == opponent && !is_alive(i, j + 1, &pieces)) {
+					possible = true;
+				}
+			}
+
+			if (possible) {
+				// Ko rule tests.
+				if (compute_hash_value() == previous_board_hash_value) {			
+					possible = false;
+				}
+				else if (all_hash_values.find(compute_hash_value()) != all_hash_values.end()) {
+					possible = false;
+				}
+			}
+
+			if (possible) {
+				// Not possible to play in one's own eye.
+				if (is_eye(i, j, player)) {
+					possible = false;
+				}
+			}
+
+			board[i][j] = empty;
+			return possible;
+		}
+		else {
+			// Not a valid position.
+			return false;
+		}
+	}
+
+	bool is_eye(int i, int j, int player) const
+	{
+		bool eye = true;
+		if (i > 0 && board[i - 1][j] != player) eye = false;
+		if (i < M - 1 && board[i + 1][j] != player) eye = false;
+		if (j > 0 && board[i][j - 1] != player) eye = false;
+		if (j < N - 1 && board[i][j + 1] != player) eye = false;
+		return eye;
+	}
+
+	void do_move(Move move)
+	{
+		depth++;
+
+		int opponent = 3 - player_to_move;
+
+		if (move == pass) {
+			player_to_move = opponent;
+			return;
+		}
+
+		std::stringstream sout;
+		for (int ii = 0; ii < M; ++ii) {
+			for (int jj = 0; jj < N; ++jj) {
+				if (board[ii][jj] == 1) sout << '1';
+				else if (board[ii][jj] == 2) sout << '2';
+				else sout << ".";
+			}
+			sout << endl;
+		}
+
+		int i,j;
+		std::tie(i, j) = ind_to_ij(move);
+		board[i][j] = player_to_move;
+
+		// We save the hash values before all captures as this is way easier
+		// to check.
+		previous_board_hash_value = compute_hash_value();
+		all_hash_values.insert(previous_board_hash_value);
+
+		// Check for the killing of any opposing stones.
+		if (i > 0 && board[i - 1][j] == opponent) {
+			check_alive(i - 1, j);
+		}
+		if (i < M - 1 && board[i + 1][j] == opponent) {
+			check_alive(i + 1, j);
+		}
+		if (j > 0 && board[i][j - 1] == opponent) {
+			check_alive(i, j - 1);
+		}
+		if (j < N - 1 && board[i][j + 1] == opponent) {
+			check_alive(i, j + 1);
+		}
+
+		std::set<std::pair<int, int>> pieces;
+		// Now the played stone must be alive.
+		if (board[i][j] != player_to_move || !is_alive(i, j, &pieces)) {
+			sout << "Moved at (" << i << ", " << j << ") player " << player_to_move << endl;
+			for (int ii = 0; ii < M; ++ii) {
+				for (int jj = 0; jj < N; ++jj) {
+					if (board[ii][jj] == 1) sout << '1';
+					else if (board[ii][jj] == 2) sout << '2';
+					else sout << ".";
+				}
+				sout << endl;
+			}
+			throw std::runtime_error(sout.str().c_str());
+		}
+		attest(board[i][j] == player_to_move);
+		attest(is_alive(i, j, &pieces));
+
+		// Next player
+		player_to_move = opponent;
+	}
+
+	bool is_alive(int i_start, int j_start, std::set<std::pair<int, int>>* pieces) const
+	{
+		if (board[i_start][j_start] == empty) {
+			// No piece here, so alive
+			return true;
+		}
+
+		std::vector<std::pair<int, int>> stack;
+		pieces->clear();
+		stack.emplace_back(i_start, j_start);
+		int player = board[i_start][j_start];
+
+		while (!stack.empty()) {
+			auto back = stack.back();
+			stack.pop_back();
+			if (pieces->find(back) != pieces->end()) {
+				// Already checked.
+				continue;
+			}
+			int i,j;
+			std::tie(i, j) = back;
+			
+			if (board[i][j] == player) {
+				pieces->emplace(i, j);
+				if (i > 0) {
+					stack.emplace_back(i - 1, j);
+				}
+				if (i < M - 1) {
+					stack.emplace_back(i + 1, j);
+				}
+				if (j > 0) {
+					stack.emplace_back(i, j - 1);
+				}
+				if (j < N - 1) {
+					stack.emplace_back(i, j + 1);
+				}
+			}
+			else if (board[i][j] == empty) {
+				// Alive.
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void check_alive(int i, int j)
+	{
+		std::set<std::pair<int, int>> pieces;
+		if (!is_alive(i, j, &pieces)) {
+			// Remove the dead pieces.
+			for (auto& ij: pieces) {
+				int i, j;
+				std::tie(i, j) = ij;
+				board[i][j] = empty;
+			}
+		}
+	}
+
+	template<typename RandomEngine>
+	void do_random_move(RandomEngine* engine)
+	{
+		auto moves = get_moves();
+		attest(! moves.empty());
+		std::uniform_int_distribution<std::size_t> move_ind(0, moves.size() - 1);
+		auto move = moves[move_ind(*engine)];
+		do_move(move);
+	}
+
+	bool has_moves() const
+	{
+		// TODO: make faster.
+		return ! get_moves().empty();
+	}
+
+	std::vector<Move> get_moves() const
+	{
+		std::vector<Move> moves;
+		if (depth > 1000) {
+			return moves;
+		}
+
+		bool opponent_has_move = false;
+		for (int i = 0; i < M; ++i) {
+		for (int j = 0; j < N; ++j) {
+			if (is_move_possible(i, j, player_to_move)) {
+				moves.push_back(ij_to_ind(i, j));
+			}
+
+			if (!opponent_has_move && is_move_possible(i, j, 3 - player_to_move)) {
+				opponent_has_move = true;
+			}
+		}}
+
+		if (moves.empty() && opponent_has_move) {
+			moves.push_back(pass);
+		}
+
+		return moves;
+	}
+
+	int get_player_score(int player) const
+	{
+		int score = 0;
+		for (int i = 0; i < M; ++i) {
+		for (int j = 0; j < N; ++j) {
+			if (board[i][j] == empty) {
+				if (is_eye(i, j, player)) {
+					score++;
+				}
+			}
+			if (board[i][j] == player) {
+				score++;
+			}
+		}}
+		return score;
+	}
+
+	double get_result(int current_player_to_move) const
+	{
+		int score1 = get_player_score(1);
+		int score2 = get_player_score(2);
+
+		if (score1 == score2) {
+			return 0.5;
+		}
+		int winner = 0;
+		if (score1 > score2) {
+			winner = 1;
+		}
+		else {
+			winner = 2;
+		}
+
+		if (winner == current_player_to_move) {
+			return 0.0;
+		}
+		else {
+			return 1.0;
+		}
+	}
+
+	void dump_board(const char* file_name) const
+	{
+		std::ofstream fout(file_name);
+		fout << "static const int M = " << M << ";" << endl;
+		fout << "static const int N = " << N << ";" << endl;
+		fout << "char board[M][N+1] = {" << endl;
+		for (int i = 0; i < M; ++i) {
+			fout << "\t\"";
+			for (int j = 0; j < N; ++j) {
+				if (board[i][j] == empty) fout << ".";
+				else fout << int(board[i][j]);
+			}
+			fout << "\"," << endl;
+		}
+		fout << "};" << endl;
+	}
+};
